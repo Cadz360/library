474a3a5dc54ad7310e39b67b3339fa173ff8d4b3
Cadz360 <173970617+Cadz360@users.noreply.github.com>
2024-11-19 08:32:09 -0800

    Book can be added to myLibrary by using add book btn and will be shown after submitting the modal form

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
+const dialog = document.querySelector("dialog");
+const showButton = document.querySelector("dialog + button");
+const closeButton = document.querySelector("dialog button");
+
+// "Show the dialog" button opens the dialog modally
+showButton.addEventListener("click", () => {
+  dialog.showModal();
+});
+
+// "Close" button closes the dialog
+closeButton.addEventListener("click", () => {
+  dialog.close();
+});
+
+const bookList = document.querySelector(".book-list");
+const bookForm = document.querySelector(".book-form");
 const myLibrary = [];
 
 function Book(title, author, numOfPages) {
@@ -11,6 +27,20 @@ function Book(title, author, numOfPages) {
 
 function addBookToLibrary(book) {
   myLibrary.push(book)
+  const newBook = document.createElement("li")
+  const newBookTitle = document.createElement("h3")
+  const newBookAuthor = document.createElement("p")
+  const newBookNumOfPages = document.createElement("p")
+
+  newBookTitle.textContent = book.title;
+  newBookAuthor.textContent = book.author;
+  newBookNumOfPages.textContent = book.numOfPages
+
+  bookList.appendChild(newBook);
+  newBook.appendChild(newBookTitle);
+  newBook.appendChild(newBookAuthor);
+  newBook.appendChild(newBookNumOfPages);
+
 }
 
 function showLibrary() {
@@ -23,6 +53,22 @@ function createNewBook(title, author, numOfPages) {
     addBookToLibrary(new Book(title, author, numOfPages));
 }
 
-createNewBook('hello', 'hello', 5)
+bookForm.addEventListener("submit", (e) => {
+  e.preventDefault();
 
-showLibrary()
+  let bookTitle = document.getElementById("book-title");
+  let bookAuthor = document.getElementById("book-author");
+  let bookNumOfPages = document.getElementById("pages-number");
+
+  if (bookTitle.value == "" || bookAuthor.value == "" || bookNumOfPages.value == ""){
+    alert("All fields need to have a valid input");
+  } else {
+    createNewBook(bookTitle.value, bookAuthor.value, bookNumOfPages.value)
+  }
+  bookTitle.value = "";
+  bookAuthor.value = "";
+  bookNumOfPages.value = "";
+  console.log(myLibrary)
+})
+
+console.log(myLibrary)
